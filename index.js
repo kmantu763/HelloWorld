82d5dbb9ff985797d42b6dbd0767821953866b65
kmantu763 <128892951+kmantu763@users.noreply.github.com>
2024-04-25 13:04:20 +0530

    db connection change again

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 
 // MongoDB connection
-const db_url = "mongodb+srv://kmantu763:mantukumar763@cluster0.io7ysrh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/HelloWorld";
+const db_url = "mongodb+srv://kmantu763:mantukumar763@mycluster.uvictff.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster/HelloWorld";
 
 mongoose.connect(db_url)
     .then(()=>console.log('db connected successfully'))
